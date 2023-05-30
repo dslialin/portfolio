2dd1c6b163abf187bde6fa9ac2ff199ec2242ecf
Aalfaa <134696508+Aalfaa@users.noreply.github.com>
2023-05-30 20:46:06 +0800

    Update and rename README.md to README.m

--- Project_1_Telecom_churn/README.m
+++ Project_1_Telecom_churn/README.m
@@ -1,12 +1,15 @@
 # Project 1. Telecom churn
 
 ## Data
+
 Table data train.csv and test.csv
 
 https://www.kaggle.com/competitions/advanced-dls-spring-2021/data
 
 ## Tasks
+
 The objective of this task is to develop a model that can predict which customers in a company are likely to remain loyal and stay with the company, and which customers are more likely to churn and switch to competitors.
 
 ## Used libraries
+  
 Pandas, numpy, matplotlib, catboost, sklearn.
